3f47b695a47a1d1b38090d6775fd29da8ed4a211
Sam Ward <sward22@eil-server.cba.ua.edu>
2022-02-10 20:22:58 -0600

    added cs class

--- Engine/app.py
+++ Engine/app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+import connectionString
+
+# initializes the Flask app
 app = Flask(__name__)
 
-
+#specifying the route and methods allowed; I think we'll just do GET, maybe something for logging in?
 @app.route('/hello/', methods=['GET', 'POST'])
+#I don't think the name of the function matters? I have no clue tbh, I guess they don't matter in .NET either...
 def welcome():
     return "Hello World!"
-    
+
+
+
+@app.route('/recommendation/', methods=['GET', 'POST'])
+def recommendItem():
+    myCs = connectionString.ConnectionString()
+    return myCs.host
+
+
+
+#from the article "This line ensures that our Flask app runs only when it is executed in the main file and not when it is imported in some other file"
+#gonna be honest idk what that means in a practical sense, but we can look into it more if we can get issues when hosting, might come up for file structure too
 if __name__ == '__main__':
+    #eventually host will be updated to our hosting service URL, can change port as needed
     app.run(host='0.0.0.0', port=8000)
---
+++ Engine/connectionString.py
@@ -0,0 +1,4 @@
+#this can be where the class ends up; I have some concerns about file structure though
+class ConnectionString:
+    def __init__(self):
+        self.host = "Example host"
