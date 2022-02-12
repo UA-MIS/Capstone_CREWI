5e48752503b1252d6da56f494eb8993ad0595b59
Sam Ward <sward22@eil-server.cba.ua.edu>
2022-02-11 19:39:49 -0600

    didn't actually get to box, but did effective restructuring

--- Client/crewi-dfa-one-page/src/App.js
+++ Client/crewi-dfa-one-page/src/App.js
@@ -1,15 +1,10 @@
-
 import './App.css';
 import MainComponent from './components/MainComponent';
-import WidgetComponent from './components/WidgetComponent';
 
 function App() {
   return (
     <div className="App">
         <MainComponent/>
-        <main>
-          <WidgetComponent/>
-        </main>
     </div>
   );
 }
