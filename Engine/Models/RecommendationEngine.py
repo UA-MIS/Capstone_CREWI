0e58831032847fdc1a7cc95e1acd62c61dd7bf06
Sam Ward <sward22@eil-server.cba.ua.edu>
2022-02-24 18:14:10 -0600

    finished

--- Engine/Models/RecommendationEngine.py
+++ Engine/Models/RecommendationEngine.py
@@ -49,18 +49,24 @@ class RecommendationEngine:
             globalStatus.addFail("PARSE_TIME_FAIL")            
             raise e
 
+    # takes in the transactions and request; this modifies the transactions directly so no return is needed
     def scoreTransactions(self, transactions, request):
         try:
+            # loading environment data
             load_dotenv()
 
+            # loops through all the transactions
             for index, transaction in enumerate(transactions):
+                # transactions start with a score of a / (index + 1); a is configurable
                 transaction.score = float(float(os.environ.get('Numerator_Constant')) / (index + 1))
+                # after getting a base score, they all get a flat constant to reduce the importance of the base score (also configurable)
                 transaction.score += float(os.environ.get('Addition_Constant'))
 
+                # if the transaction does not match the request user, reduce the score by a "reducer" (just a multiplier that is less than 1, not a technical term; configurable)
                 if transaction.userId != request.userId : transaction.score *= float(os.environ.get('Other_User_Reducer'))
+                # if the request location matches the transaction's store's location, increase the score by a multiplier (configurable)
                 if transaction.storeId == request.storeId : transaction.score *= float(os.environ.get('Matching_Store_Multiplier'))
-
-
+                
         except Exception as e:
             # print issue to terminal and update status
             printFormatting.printError(str(e))
